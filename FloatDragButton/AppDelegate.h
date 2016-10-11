e50941f063430da82239f895c0eae5ed18929851
rocky <fengjian0213@126.com>
2016-10-11 11:37:59 +0800

    Initial Commit

---
+++ FloatDragButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FloatDragButton
+//
+//  Created by rocky on 16/10/11.
+//  Copyright © 2016年 RockyFung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FloatDragButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FloatDragButton
+//
+//  Created by rocky on 16/10/11.
+//  Copyright © 2016年 RockyFung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
